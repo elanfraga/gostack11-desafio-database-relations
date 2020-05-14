a6702993871658a00e5fe8fa8d5b82bc3e609b13
Elan Fraga <elanfraga@gmai.com>
2020-05-14 00:25:16 -0300

    create customer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import { userInfo } from 'os';
 import Customer from '../infra/typeorm/entities/Customer';
 import ICustomersRepository from '../repositories/ICustomersRepository';
 
@@ -15,7 +16,20 @@ class CreateCustomerService {
   constructor(private customersRepository: ICustomersRepository) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkCustomerExists = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (checkCustomerExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const user = this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return user;
   }
 }
 
